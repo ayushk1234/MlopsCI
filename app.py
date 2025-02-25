b1c6c082044121b61b083c13fc180b9caacf5098
ayushk1234 <ayushk527@gmail.com>
2025-02-25 10:51:59 +0530

    docker

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
-import streamlit as st
+# import streamlit as st
 
-# Streamlit UI
-st.title("Power Calculator")
-st.write("Enter a number to calculate its square, cube, and fifth power.")
+# # Streamlit UI
+# st.title("Power Calculator")
+# st.write("Enter a number to calculate its square, cube, and fifth power.")
 
-# Get user input
-n = st.number_input("Enter an integer", value=1, step=1)
+# # Get user input
+# n = st.number_input("Enter an integer", value=1, step=1)
 
-# Calculate results
-square = n ** 2
-cube = n ** 3
-fifth_power = n ** 5
+# # Calculate results
+# square = n ** 2
+# cube = n ** 3
+# fifth_power = n ** 5
 
-# Display results
-st.write(f"The square of {n} is: {square}")
-st.write(f"The cube of {n} is: {cube}")
-st.write(f"The fifth power of {n} is: {fifth_power}")
+# # Display results
+# st.write(f"The square of {n} is: {square}")
+# st.write(f"The cube of {n} is: {cube}")
+# st.write(f"The fifth power of {n} is: {fifth_power}")
 
 
+from flask import Flask,render_template,request 
+
+app =Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return '''
+        <html>
+        <body>
+            <form action="/greet" method="POST">
+                Enter your name : <input type ="text" name="username">
+                <input type= "submit" value ="Submit">
+            </form>
+        </body>
+        </html>
+    '''
+@app.route('/greet',methods=['POST'])
+def greet():
+    user_input = request.form['username']
+    return f"Hello {user_input},Welcome to this app for Docker demo "
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=5001)
