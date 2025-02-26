f6759d0eb1d429af2903cdb690ae4d87bb291d24
ayushk1234 <ayushk527@gmail.com>
2025-02-26 11:16:22 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ayush Khandelwal",
+    author_email="ayushk527@gmail.com",
+    packages=find_packages()
+)
